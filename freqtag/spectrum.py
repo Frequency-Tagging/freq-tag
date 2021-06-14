be55afc4fc87923d94529cfc281c2fd74a7ac410
kalenkovich <e.kalenkovich@gmail.com>
2021-06-14 10:45:24 +0300

    use numpy, not mne, for Fourier calculations
    Obviously, mne uses numpy's (well, maybe scipy's) fft under the hood. This way, we don't rely on mne for something that simple.

--- freqtag/spectrum.py
+++ freqtag/spectrum.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 import numpy as np
 
 
@@ -16,20 +18,33 @@ class Spectrum(object):
 
         :param epochs: mne.Epochs object
         :param tmin: minimum time of interest
-        :param tmax: maximum time of interest
+        :param tmax: maximum time of interest (the sample at this time is not
+         included)
         :param fmin: minimum frequency of interest
         :param fmax: maximum frequency of interest
         :return: an object of type Spectrum
         """
-        from mne.time_frequency import psd_welch
+        # Prepare data
+        data = epochs.get_data()
+        time_mask = (tmin <= epochs.times) & (epochs.times < tmax)
 
+        # Fourier
         n_fft = int(epochs.info['sfreq'] * (tmax - tmin))
-        psd, frequencies = psd_welch(
-            epochs,
-            tmin=tmin, tmax=tmax,
-            fmin=fmin, fmax=fmax,
-            # These result in plain FFT: single window, no windowing function
-            n_fft=n_fft, n_overlap=0, n_per_seg=None, window='boxcar',
-            verbose=False)
+        F = np.fft.rfft(data[..., time_mask], axis=-1, norm='ortho')
+
+        # Calculate power
+        power = np.abs(F) ** 2
+        # power at avery frequency but the DC and Nyqvist should be multiplied by two
+        nyqvist_index = ceil(n_fft / 2)
+        power[1:nyqvist_index] *= 2
+
+        # We are calculating "density" so we need to convert power to "1/Hz" units.
+        psd = power / epochs.info['sfreq']
+
+        # Calculate frequencies and remove the unnecessary ones
+        frequencies = np.fft.rfftfreq(n=n_fft, d=(1 / epochs.info['sfreq']))
+        frequency_mask = (fmin <= frequencies) & (frequencies <= fmax)
+        psd = psd[..., frequency_mask]
+        frequencies = frequencies[frequency_mask]
 
         return cls(data=psd, frequencies=frequencies, kind='psd')
